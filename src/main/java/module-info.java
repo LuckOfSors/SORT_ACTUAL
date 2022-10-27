1759b58f4f37e6f588e1e91d5d47aa9d21897b17
Joe Volpe <volpe06@outlook.com>
2022-10-27 14:26:11 -0400

    COMMIT

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.sort_actual {
     requires javafx.controls;
     requires javafx.fxml;
     requires unirest.java;
+    requires com.google.gson;
 
 
     opens volpe.kennedy.sort_actual to javafx.fxml;
